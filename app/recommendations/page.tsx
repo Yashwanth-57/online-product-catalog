d907c887c87c5dd0ac7c9bb1c608662dc0110307
Yashwanth <yashwanth567@gmail.com>
2025-11-02 01:20:59 +0530

    fix: base URL for Render build

--- app/recommendations/page.tsx
+++ app/recommendations/page.tsx
@@ -19,7 +19,7 @@ export default function RecommendationsPage() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-       const res = fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
+       const res =  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
 
         if (!res.ok) throw new Error("Failed to fetch products");
         const data = await res.json();
